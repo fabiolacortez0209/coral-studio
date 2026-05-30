96d5ef028d7416cbb73406e9525e894af57adebe
fabiolacortez0209 <fabiolacortez.0209@gmail.com>
2026-05-29 19:12:51 -0700

    Add templates images

--- data/templates.ts
+++ data/templates.ts
@@ -4,35 +4,35 @@ export const templates = [
     title: "Encanto",
     category: "XV Años",
     href: "/xv-anos/encanto",
-    image: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1",
+    image: "/templates/encanto.png",
   },
 
   {
     title: "Carmín",
     category: "XV Años",
     href: "/xv-anos/carmin",
-    image: "https://images.unsplash.com/photo-1519741497674-611481863552",
+    image: "/templates/carmin.png",
   },
 
   {
     title: "Esmeralda",
     category: "XV Años",
     href: "/xv-anos/esmeralda",
-    image: "https://images.unsplash.com/photo-1492684223066-81342ee5ff30",
+    image: "/templates/esmeralda.png",
   },
 
   {
     title: "Celeste",
     category: "XV Años",
     href: "/xv-anos/celeste",
-    image: "https://images.unsplash.com/photo-1511285560929-80b456fea0bc",
+    image: "/templates/celeste.png",
   },
 
   {
     title: "Lavanda",
     category: "XV Años",
-    href: "/xv-anos/Lavanda",
-    image: "https://images.unsplash.com/photo-1515934751635-c81c6bc9a2d8",
+    href: "/xv-anos/lavanda",
+    image: "/templates/lavanda.png",
   },
 
 ];
